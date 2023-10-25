510ba19949fdad57bd3e253906b4e0facf1d50b8
Tyler <sekie0@gmail.com>
2023-10-24 23:38:00 -0600

    s11 chal3

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Spinner from './Spinner.js'; // adjust the path
+import Spinner from './Spinner.js';
 
 test('Spinner renders without crashing', () => {
   render(<Spinner />);
 });
-
-// Add more tests based on different props and expected behavior
